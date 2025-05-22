9822daaf7ded72a1c34de0dff017e8fe4551160a
itomasevich <itomasevich@accionpoint.com>
2025-05-22 01:33:37 -0300

    minor update

--- tests/test_ortogonalidad.py
+++ tests/test_ortogonalidad.py
@@ -6,25 +6,37 @@ from vectores.ortogonalidad import son_ortogonales
 class TestOrtogonalidad(unittest.TestCase):
 
     def test_ortogonales_en_2d(self):
+        """
+        Verifica verdadero para ortogonal [0, 1] y [1, 0]
+        """
         u = [1, 0]
         v = [0, 1]
         t = 1e-10
-        self.assertTrue(son_ortogonales(u, v, t))
+        self.assertTrue(son_ortogonales(u, v, t,), "Verdadero")
 
     def test_no_ortogonales(self):
+        """
+        Verifica falso para ortogonal [1, 2] y [3, 4]
+        """
         u = [1, 2]
         v = [3, 4]
         t = 1e-10
-        self.assertFalse(son_ortogonales(u, v, t))
+        self.assertFalse(son_ortogonales(u, v, t), "Falso")
 
     def test_casi_ortogonales_con_tolerancia(self):
+        """
+        Verífica verdadero para casi ortogonal con tolerancia [1, 0.00000000001] y [0, 1]
+        """
         u = [1, 0.00000000001]
         v = [0, 1]
         t = 1e-10
-        self.assertTrue(son_ortogonales(u, v, t))  # porque el producto punto ≈ 1e-11
+        self.assertTrue(son_ortogonales(u, v, t), "Verdadero porque el producto punto ≈ 1e-11")
 
     def test_casi_ortogonales_fuera_de_tolerancia(self):
+        """
+        Verífica verdadero para casi ortogonal con tolerancia [1, 0.001] y [0, 1]
+        """
         u = [1, 0.001]
         v = [0, 1]
         t = 1e-10
-        self.assertFalse(son_ortogonales(u, v, t))  # porque 0.001 > tolerancia
+        self.assertFalse(son_ortogonales(u, v, t), "Falso porque 0.001 > tolerancia")
